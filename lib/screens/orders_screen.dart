7557a5dd80085e74b4f19f6fd6b5a1bf685e8746
eSaniello <shaniel29samadhan@gmail.com>
2020-02-18 00:24:19 -0300

    Working on deleting and updating datatable

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -10,7 +10,7 @@ class OrdersScreen extends StatefulWidget {
 class _OrdersScreenState extends State<OrdersScreen> {
   final Firestore firestore = fb.firestore();
 
-  void _showDeleteDialog(var ds) {
+  void _showDeleteDialog(var ds, var i) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -28,10 +28,11 @@ class _OrdersScreenState extends State<OrdersScreen> {
               child: Text('Confirm'),
               onPressed: () {
                 setState(() {
-                  firestore.collection('orders').doc(ds['id']).delete();
-                  orders.remove(ds);
-                  Navigator.pop(context);
-                  Navigator.pop(context);
+                  // firestore.collection('orders').doc(ds['id']).delete();
+                  // orders.remove(ds);
+                  // // ordersDataRows.removeAt(i - 1);
+                  // Navigator.pop(context);
+                  print(i);
                 });
               },
             )
@@ -42,12 +43,14 @@ class _OrdersScreenState extends State<OrdersScreen> {
   }
 
   List orders = [];
+  List ordersDataRows = [];
 
   @override
   void initState() {
     super.initState();
 
     firestore.collection('orders').get().then((order) {
+      int i = 0;
       order.forEach((o) {
         firestore
             .collection('customers')
@@ -60,7 +63,7 @@ class _OrdersScreenState extends State<OrdersScreen> {
               .get()
               .then((prod) {
             setState(() {
-              orders.add({
+              var order = {
                 'id': o.id,
                 'customer': cus.data(),
                 'product': prod.data(),
@@ -68,7 +71,61 @@ class _OrdersScreenState extends State<OrdersScreen> {
                 'quantity': o.data()['quantity'],
                 'size': o.data()['size'],
                 'status': o.data()['status'],
-              });
+              };
+              orders.add(order);
+
+              i++;
+
+              DataRow row = DataRow(cells: [
+                DataCell(Text('$i')),
+                DataCell(Text(
+                  '${cus.data()['firstname']} ${cus.data()['lastname']}',
+                )),
+                DataCell(Text(
+                  '${prod.data()['name']}',
+                )),
+                DataCell(Text(
+                  '${prod.data()['design']}',
+                )),
+                DataCell(Text(
+                  '${o.data()['size']}',
+                )),
+                DataCell(Text(
+                  '${o.data()['quantity']}',
+                )),
+                DataCell(Text(
+                  '${o.data()['address']}',
+                )),
+                DataCell(Text(
+                  '${cus.data()['mobile']}',
+                )),
+                DataCell(
+                  Text(
+                    '${o.data()['status']}',
+                  ),
+                ),
+                DataCell(
+                  IconButton(
+                    icon: Icon(Icons.edit),
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(
+                        '/edit_order',
+                        // arguments: orders[index],
+                      );
+                    },
+                  ),
+                ),
+                DataCell(
+                  IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      _showDeleteDialog(order, i);
+                    },
+                  ),
+                ),
+              ]);
+
+              ordersDataRows.add(row);
             });
           });
         });
@@ -76,51 +133,67 @@ class _OrdersScreenState extends State<OrdersScreen> {
     });
   }
 
-  Color iconColor = Colors.black;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          itemCount: orders.length,
-          itemBuilder: (BuildContext context, int index) {
-            return orders.isEmpty
-                ? CircularProgressIndicator()
-                : ListTile(
-                    leading: Icon(
-                      Icons.assignment,
-                      color: iconColor,
-                    ),
-                    title: Text(orders[index]['customer']['firstname']),
-                    subtitle: Text(orders[index]['product']['name']),
-                    trailing: PopupMenuButton(
-                      color: Colors.white,
-                      itemBuilder: (context) => [
-                        PopupMenuItem(
-                          value: 1,
-                          child: FlatButton(
-                            child: Text('Edit'),
-                            onPressed: () {
-                              Navigator.of(context).pushNamed(
-                                '/edit_order',
-                                arguments: orders[index],
-                              );
-                            },
-                          ),
-                        ),
-                        PopupMenuItem(
-                          value: 2,
-                          child: FlatButton(
-                            child: Text('Delete'),
-                            onPressed: () {
-                              _showDeleteDialog(orders[index]);
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-          }),
+      body: DataTable(
+        columns: [
+          DataColumn(numeric: true, label: Text('#')),
+          DataColumn(label: Text('Customer')),
+          DataColumn(label: Text('Product')),
+          DataColumn(label: Text('Design')),
+          DataColumn(label: Text('Size')),
+          DataColumn(numeric: true, label: Text('Quantity')),
+          DataColumn(label: Text('Address')),
+          DataColumn(numeric: true, label: Text('Mobile')),
+          DataColumn(label: Text('Status')),
+          DataColumn(label: Text('Edit')),
+          DataColumn(label: Text('Delete')),
+        ],
+        rows: List.unmodifiable(() sync* {
+          yield* ordersDataRows;
+        }()),
+      ),
+      // body: ListView.builder(
+      //     itemCount: orders.length,
+      //     itemBuilder: (BuildContext context, int index) {
+      //       return orders.isEmpty
+      //           ? CircularProgressIndicator()
+      //           : ListTile(
+      //               leading: Icon(
+      //                 Icons.assignment,
+      //                 color: Colors.black,
+      //               ),
+      //               title: Text(orders[index]['customer']['firstname']),
+      //               subtitle: Text(orders[index]['product']['name']),
+      //               trailing: PopupMenuButton(
+      //                 color: Colors.white,
+      //                 itemBuilder: (context) => [
+      //                   PopupMenuItem(
+      //                     value: 1,
+      //                     child: FlatButton(
+      //                       child: Text('Edit'),
+      //                       onPressed: () {
+      //                         Navigator.of(context).pushNamed(
+      //                           '/edit_order',
+      //                           arguments: orders[index],
+      //                         );
+      //                       },
+      //                     ),
+      //                   ),
+      //                   PopupMenuItem(
+      //                     value: 2,
+      //                     child: FlatButton(
+      //                       child: Text('Delete'),
+      //                       onPressed: () {
+      //                         _showDeleteDialog(orders[index]);
+      //                       },
+      //                     ),
+      //                   ),
+      //                 ],
+      //               ),
+      //             );
+      //     }),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
